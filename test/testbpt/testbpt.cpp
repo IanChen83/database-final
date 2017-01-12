9509bc005f1bf29d3928120807f76467c6cbbd0d
Allen Lin <allen830423@gmail.com>
2017-01-12 23:09:54 +0800

    add tree insert and search test

--- test/testbpt/testbpt.cpp
+++ test/testbpt/testbpt.cpp
@@ -1,11 +1,34 @@
+#include <string>
 #include "gtest/gtest.h"
+#define UNIT_TEST 0
 #include "bpt.h"
 
 using namespace std;
 
-TEST(Bpt, key_binary_search) {
-    
+TEST(Bpt, tree_initialize) {
+    bplus_tree *tree = new bplus_tree(5, 10 ,10);
+    Value a(1);
+    EXPECT_EQ(tree->level, 5);
+    EXPECT_EQ(tree->entries, 10);
+    EXPECT_EQ(tree->order, 10);
 }
+
+TEST(Bpt, tree_insert) {
+    bplus_tree *tree = new bplus_tree(5, 10 ,10);
+    Value a(1);
+    tree->bplus_tree_insert(a, 1);
+    bplus_non_leaf *root = (bplus_non_leaf *)tree->root;
+    EXPECT_EQ(root->key[0].IntValue, 1);
+}
+
+TEST(Bpt, tree_search) {
+    bplus_tree *tree = new bplus_tree(5, 10 ,10);
+    Value a(1);
+    tree->bplus_tree_insert(a, 1);
+    bplus_non_leaf *root = (bplus_non_leaf *)tree->root;
+    EXPECT_EQ(root->key[0].IntValue, 1);
+}
+
 
 
 
